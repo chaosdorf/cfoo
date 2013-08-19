cb61837ab2f396938798cea1864559c1fffb0d17
bison-- <mike.bison42@googlemail.com>
2013-08-19 02:14:49 +0200

    only run if compilation was successful

--- sic/compile.sh
+++ sic/compile.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 gcc -Wall -o sic si.c player.c
-chmod +x sic
-./sic
+RESULT=$?
+
+if [ $RESULT -ne 0 ]
+then
+	echo "**** compile error: $RESULT ****"
+else
+	echo "**** compilation OK ****"
+	chmod +x sic
+	./sic
+fi
+
