89be0cdbc7053314743c0803c512d809d3786a1f
bison-- <mike.bison42@googlemail.com>
2013-11-24 18:35:44 +0100

    simple basic bench

---
+++ bench.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#include <time.h>
+#include <sys/time.h> 
+
+int innerLoops =  1000000000;
+struct timeval startTime;
+
+int main(int argc, char *args[]) {
+	for (int i = 0; i<5; i++) {
+		startTimer();
+		testFor();
+		stopTimer();
+	}
+}
+
+void startTimer() {
+	gettimeofday(&startTime, NULL);
+}
+
+void stopTimer() {
+	struct timeval stopTime;
+	gettimeofday(&stopTime, NULL);
+	double total;
+	
+	total = (stopTime.tv_sec - startTime.tv_sec) * 1000.0;      // sec to ms
+	total += (stopTime.tv_usec - startTime.tv_usec) / 1000.0;   // us to ms
+	
+	printf("%f\n", total);
+}
+
+void testFor() {
+	int dummyVar = 0;
+	for (int i = 0;i < innerLoops; i++) {
+		dummyVar = !dummyVar;
+	}
+}
